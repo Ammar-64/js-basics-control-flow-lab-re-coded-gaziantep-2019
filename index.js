d3f6cf8dd9533d83f88113efb49dcb9f59ccc488
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-06-16 11:57:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let distance
+  if (distance=<199) {
+    distance = 'This one is on me!';
+  } else if (distance=<2001) {
+    distance = 'I will gladly take your thirty bucks.';
+  }
+    else {
+      distance = 'No can do.';
+    }
+  }
 }
 
 function ternaryCheckCity(){
